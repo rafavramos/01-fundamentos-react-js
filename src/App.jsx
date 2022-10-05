a800e35ac7ecfffbdac23accf5dd6954f8a312a3
Rafael Ramos <rafaelramos@unesc.net>
2022-10-05 15:11:30 -0300

    feat: First Props

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import { Post } from './Post'
 export function App() {
   return (
     <div>
-      <Post /> <Post /> <Post /> <Post />
+      <Post
+        author="Rafael"
+        content="Em pleno 2022, ano do Elon Musk. Marcha nos projetos. Eu sou apenas um mero camponês."
+      />
     </div>
   )
 }
--- src/Post.jsx
+++ src/Post.jsx
@@ -1,8 +1,16 @@
-export function Post() {
-  return <p>Post</p>
+export function Post(props) {
+  return (
+    <div>
+      <strong>{props.author}</strong>
+      <p>{props.content}</p>
+    </div>
+  )
 }
 
 // Default Exports - export no final
 // Vantagens: Pode nomear o componente na importação
 
 // Named Exports - Usa o export na função
+
+//Props - Propriedades são utilizadas para reaproveitar o componente utilizando propriedades difentes na exibição
+//props: {author, content}
