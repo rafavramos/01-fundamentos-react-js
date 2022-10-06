69e85ca15eee4860f9e2c7882ca6d5a0c5f9ab94
Rafael Ramos <rafaelramos@unesc.net>
2022-10-06 20:00:44 -0300

    feat: Primeiro contato com Css Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { Header } from './components/Header'
 import { Post } from './Post'
+import './styles.css'
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post
         author="Rafael"
         content="Em pleno 2022, ano do Elon Musk. Marcha nos projetos. Eu sou apenas um mero camponês."
