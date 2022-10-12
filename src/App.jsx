a0b9495a1a84c21c8be6f2aa51696ca8a5c74c5a
Rafael Ramos <rafaelramos@unesc.net>
2022-10-12 19:53:38 -0300

    feat: Posts with props and install date-fns

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,44 @@ import './global.css'
 import styles from './App.module.css'
 import { Post } from './components/Post'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https:github.com/rafavramos.png',
+      name: 'Rafael Ramos',
+      role: 'Full-Stack Developer @NewSpace'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala Pessoal' },
+      {
+        type: 'paragraph',
+        content:
+          'Em pleno 2022, ano da liberdade. Só o caráter vence o shape. Marcha nos projetos. Treine, trabalhe e estude. Cheguei calado ergui meus pesos e fui embora.'
+      },
+      { type: 'link', content: '#rocketseat #shapedealien #shapefalante' }
+    ],
+    publishedAt: new Date('2022-05-03 10:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https:github.com/JoaoVitorAlvesR.png',
+      name: 'Joa Vitor Alves',
+      role: 'Developer @Procer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala Pessoal' },
+      {
+        type: 'paragraph',
+        content: 'Em pleno 2022, era da liberdade. Quero um amor de verdade.'
+      },
+      { type: 'link', content: '#rocketseat #frango #procer' }
+    ],
+    publishedAt: new Date('2022-09-03 22:00:00')
+  }
+]
+
 export function App() {
   return (
     <div>
@@ -11,8 +49,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,41 +1,52 @@
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 import styles from './Post.module.css'
 import { Comment } from './Comment'
 import { Avatar } from './Avatar'
 
-export function Post() {
+export function Post({ author, publishedAt, content }) {
+  const publishedDateFormatted = format(
+    publishedAt,
+    "d 'de' LLLL 'às' HH:mm'h'",
+    { locale: ptBR }
+  )
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true
+  })
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://avatars.githubusercontent.com/u/81692502?v=4" />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Rafael Ramos</strong>
-            <span>Full Stack Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title="11 de maio Às 8:13" dateTime="2022-05-11 08:13:00">
-          Publicado a 1h
+        <time
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
+          {publishedDateRelativeToNow}
         </time>
       </header>
 
       <div className={styles.content}>
-        <p>
-          Em pleno 2022, ano de copa do mundo. Eu sou apenas um mero camponês.
-          Botei o shape em jogo. Dei um perfil fake meu pra elas seguirem, quero
-          um amor de verdade não uma rata que me quer pelo Toguro que sou.
-        </p>
-        <p>
-          Em pleno 2022, ano do amor. Dei um perfil fake meu pra elas seguirem,
-          quero um amor de verdade não uma rata que me quer pelo Toguro que sou.
-        </p>
-        <p>
-          Em pleno 2022, ano de eleição. Pré-treino bateu. Botei o shape em
-          jogo. Quero um amor de verdade. Acredite nos seus sonhos.
-        </p>
-        <p>
-          <a href="">#rocketseat #shapedealien #shapefalante</a>
-        </p>
+        {content.map(line => {
+          if (line.type === 'paragraph') {
+            return <p>{line.content}</p>
+          } else if (line.type === 'link') {
+            return (
+              <p>
+                <a href="#">{line.content}</a>
+              </p>
+            )
+          }
+        })}
       </div>
 
       <form className={styles.commentForm}>
